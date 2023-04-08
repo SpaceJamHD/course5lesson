7699c15661a909180911aae11e377302ad033ab0
*Ilya <fedaevi52@gmail.com>
2023-04-08 21:32:38 +0300

    create js tags in HTML

--- js.js
+++ js.js
@@ -4,12 +4,30 @@ const age = 30;
 
 let str;
 
-str = 'Hello my name is' + firstName + ' ' lastName;
+str = 'Hello my name is' + firstName + ' ' + lastName;
+
+//ES5
 
 str = '<ul>' +
     '<li>firstName' + firstName + '</li>' +
     '<li>lastName' + lastName + '</li>' +
     '<li>Age:' + age + '</li>'
-'<ul>';
+'</ul>';
 
-console.log(str);
+//console.log(str);
+
+// ES6
+
+str = `
+
+<ul>
+
+<li>First name: ${firstName}</li>
+<li>last name: ${lastName}</li>
+<li>Age: ${age}</li>
+<li>Math.random: ${Math.random()}</li>
+<li>5 + 5 = ${5 + 5}</li>
+</ul>
+`;
+
+document.body.innerHTML = str;
